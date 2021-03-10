5d625ea7bde4a02dba2c69e1ebd143492ff7f98b
mirawinkel <mirawinkel206@gmail.com>
2021-03-10 03:12:22 -0800

    added work in progress to life pair

--- src/lifePair.js
+++ src/lifePair.js
@@ -1,5 +1,14 @@
 
 
 function lifePair() {
-
+    question.innerHTML = 'Function Still in Progress';
+    button1.innerHTML = 'Still';
+    button2.innerHTML = 'Needs';
+    button3.innerHTML = 'Building';
+    button1.removeEventListener('click', foodPair);
+    button2.removeEventListener('click', winePair);
+    button3.removeEventListener('click', lifePair);
+    button1.addEventListener('click', apps);
+    button2.addEventListener('click', main);
+    button3.addEventListener('click', dessert);
 }
